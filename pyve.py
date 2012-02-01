b880a02c227eda559152b6d4daaf7a78308f8808
Radosław Szalski <radoslaw.szalski@gmail.com>
2012-02-01 18:51:06 +0100

    Adds basic mysql backup functionality

---
+++ pyve.py
@@ -0,0 +1,47 @@
+import os, glob, configparser, shlex
+import subprocess as subp
+
+def backupMysql(config = None):
+    ''' Performs a MySQL backup using mysqldump tool 
+        and a specific config file
+
+        Dependancies: mysqldump
+    '''
+    print('o Performing MySQL Backup...')
+    print('\t=> Checking for the mysqldump tool...')
+
+    try:
+        with open(os.devnull, 'w') as devnull:
+            if subp.call(['which', 'mysqldump'], stdout = devnull, stderr = devnull) == 1:
+                print('\t=> mysqldump NOT found. Aborting.')
+                return
+    except Exception as ex:
+        print('\t=> An exception occured. Unable to use "which"')
+        print(ex)
+        return
+    
+    print('\t=> mysqldump found. Proceeding with backup...')
+    
+    with open(os.path.join(config['path'], config['backup_name']), 'w') as backupFile:
+        subp.call(shlex.split('mysqldump -h {host} -u {username} -p{password} --databases {databases}'.format(**config)), stdout = backupFile)
+    print('\t=> MySQL Backup Finished.')
+
+# Available backup routines for different sources: mysql, files, ftp etc
+backupRoutines = {
+    'mysql': backupMysql,
+}
+
+# Finds the path to configuration files
+if os.path.isabs(__file__):
+    configPath = os.path.join(path, 'configs')
+else:
+    configPath = os.path.join(os.path.dirname(os.path.join(os.getcwd(), __file__)), 'configs')
+
+if __name__ == '__main__':
+    for configFile in glob.iglob(configPath +  '/*'):
+        config = configparser.ConfigParser()
+        config.read(configFile)
+        
+        for section in config.sections():
+            backupRoutines[section](config[section])
+
