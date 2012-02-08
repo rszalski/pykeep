0bccffee85a890b5536d68ad6987fe5c75b3cd50
Radosław Szalski <radoslaw.szalski@gmail.com>
2012-02-09 00:07:39 +0100

    Separates the argument parser setup into a different module

---
+++ argparser.py
@@ -0,0 +1,5 @@
+import argparse
+
+parser = argparse.ArgumentParser(description = 'Tool for automating MySQL and FTP backups')
+parser.add_argument('--dry-run', '-d', dest = 'dryRun', nargs = '?', const = True, default = False, \
+                    help = 'Runs the script in dry run mode, when no connections, file manipulations etc. are performed')
