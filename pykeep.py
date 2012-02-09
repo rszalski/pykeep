7b0cdb75f8d0efb5aedd7265845cc89c6f68f992
Radosław Szalski <radoslaw.szalski@gmail.com>
2012-02-09 23:33:11 +0100

    Refactors the configFiles section

--- pykeep.py
+++ pykeep.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     configFiles = [name for name in os.listdir(config.__USER_CONFIG_PATH__) if os.path.isfile(os.path.join(config.__USER_CONFIG_PATH__, name))]
 
-    if len(configFiles) == 0:
-        print('No config files found in {path}. Nothing to do! Exiting...'.format(path = config.__USER_CONFIG_PATH__))
+    if not configFiles:
+        print('No config files found in {}. Nothing to do! Exiting.'.format(config.__USER_CONFIG_PATH__))
     else:
         for configFile in configFiles:
             userConfig = configparser.ConfigParser()
